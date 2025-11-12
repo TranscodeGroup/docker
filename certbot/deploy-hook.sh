85cf31dc04c0d80389285c85aba3dd9535074d09
th-ci <root@tg.th-ci>
2025-11-12 14:18:39 +0700

    wip: 添加自动申请证书的demo和说明

--- certbot/deploy-hook.sh
+++ certbot/deploy-hook.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-cp "$RENEWED_LINEAGE/fullchain.pem" "$RENEWED_LINEAGE/certificate.crt"
-cp "$RENEWED_LINEAGE/privkey.pem" "$RENEWED_LINEAGE/certificate.key"
+cp -f "$RENEWED_LINEAGE/fullchain.pem" "$RENEWED_LINEAGE/certificate.crt"
+cp -f "$RENEWED_LINEAGE/privkey.pem" "$RENEWED_LINEAGE/certificate.key"
 
-echo "请保证将证书变量设置为:"
-echo "SSL_CERTIFICATE=${DATA_DIR:-/data}/certbot/live/$(basename "$RENEWED_DOMAINS")/certificate"
+echo "======================"
+echo "请将证书变量设置为:"
+echo "SSL_CERTIFICATE='${DATA_DIR:-/data}/certbot/live/$(basename "$RENEWED_LINEAGE")/certificate'"
+echo
 
 echo "重启Nginx..."
 docker compose -p "${COMPOSE_PROJECT_NAME}" restart nginx
+echo "重启完成"
+echo "======================"
