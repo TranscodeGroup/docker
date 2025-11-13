ce416da87be9eae6e9df8811125ce253813711c6
niexiaorong <fukua365@gmail.com>
2025-11-13 15:50:52 +0800

    feat(初始化分区): 自动初始化分区

--- mysql8/initdb/100-init-partition-table.sql
+++ mysql8/initdb/100-init-partition-table.sql
@@ -1,3 +1,7 @@
+USE maintain;
+CALL create_partition_by_month('maintain', 'jtt808_position');
+CALL create_partition_by_month('maintain', 'jtt808_alarm');
+
 USE cbus;
 CALL create_partition_by_month('gps', 'device_position');
 CALL create_partition_by_month('gps', 'device_depart_arrive');
